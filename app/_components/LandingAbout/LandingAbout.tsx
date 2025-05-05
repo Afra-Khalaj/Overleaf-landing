79efa9acc1cc821e8babc007995b25ad45abde70
Fateme Khalaj <fateme.m.a.khalaj@gmail.com>
2025-05-05 14:43:05 +0330

    about us on landing background modified

--- app/_components/LandingAbout/LandingAbout.tsx
+++ app/_components/LandingAbout/LandingAbout.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import React from "react";
 
 const LandingAbout = () => {
+  const isRTL = document?.documentElement?.lang === "fa";
+
   return (
     <div className="flex justify-center">
       <div
-        className="h-[80vh] mt-60 w-[100%] p-24 bg-cover bg-center shadow-inner"
-        style={{ backgroundImage: `url('/background-green-grey.png')` }}
+        className="h-[80vh] mt-60 w-full p-24 bg-cover bg-center shadow-inner"
+        style={{
+          backgroundImage: `url('/background-green-grey.png')`,
+          transform: isRTL ? "scaleX(-1)" : "none",
+        }}
       >
-        <h1 className="text-neutral text-4xl font-bold">چرا پلتفرم ما؟</h1>
+        <div style={{ transform: isRTL ? "scaleX(-1)" : "none" }}>
+          <h1 className="text-neutral text-4xl font-bold">چرا پلتفرم ما؟</h1>
+        </div>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html
-      lang="fa"
+      lang="en"
       dir="ltr"
       className={`${playfair.variable} ${sourceSans.variable}`}
     >
